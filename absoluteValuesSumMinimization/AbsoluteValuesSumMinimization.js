d716c3c1c8dce178820e36854eb5dc880a95f899
Sobiah Selvarajah <sobiahselvarajah@Sobiahs-MacBook-Pro.local>
2021-07-15 22:21:49 +0100

    template for js and ts added

---
+++ absoluteValuesSumMinimization/AbsoluteValuesSumMinimization.js
@@ -0,0 +1,9 @@
+function absoluteValuesSumMinimization(a) {
+    return 5;
+}
+
+// console.log(absoluteValuesSumMinimization([2, 4, 7]));
+// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+
