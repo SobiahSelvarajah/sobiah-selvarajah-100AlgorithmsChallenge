f1060dcfa18568950e64f1db54d0f1cba81d274c
Sobiah Selvarajah <sobiahselvarajah@Sobiahs-MacBook-Pro.local>
2021-07-17 22:13:21 +0100

    for loop method done to summ all numbers

--- add/Add.ts
+++ add/Add.ts
@@ -11,3 +11,23 @@ console.log(addNumbers( 25, 187));
 
 //! Write function -> returns sum of all numbers regardless of # of params.
 
+function sumOfNumbers(numArray: number[]): number {
+
+
+    // ---- Method 1 ---- for loop
+    // loop through the array
+    // set result as 0 initially
+    // keep adding the next number in the array to the result
+    // until array length is reached
+
+    let result = 0;
+    
+    for(let i=0; i < numArray.length; i++) {
+        result = numArray[i] + result
+    }
+    return result
+}
+
+console.log(sumOfNumbers([4, 1, 9, 7]));
+console.log(sumOfNumbers([88, 30, 29]));
+
