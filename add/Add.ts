9dd58c9ffa65351da34f53433cafe4b85bd80268
Sobiah Selvarajah <sobiahselvarajah@Sobiahs-MacBook-Pro.local>
2021-07-17 07:31:04 +0100

    function to add numbers done

---
+++ add/Add.ts
@@ -0,0 +1,13 @@
+
+//! write function to add 2 numbers
+
+function addNumbers(num1: number, num2: number) {
+    return num1 + num2;
+}
+
+console.log(addNumbers( 5, 6));
+console.log(addNumbers( 25, 187));
+
+
+//! Write function -> returns sum of all numbers regardless of # of params.
+
